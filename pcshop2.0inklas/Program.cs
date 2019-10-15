5f4aee4d9818842172fee1a3855876c8c933058d
Enrick De Munter <enrick.demunter@student.romerocollege.be>
2019-10-15 10:04:48 +0200

    Add project files.

---
+++ pcshop2.0inklas/Program.cs
@@ -0,0 +1,34 @@
+﻿// Enrick De Munter 5ITN nr4 2019/10/05
+/* 
+ * Analyse
+ *Vraag welk artikel de persoon wil
+ *vraag Hvl stuks
+ *vraag wat is de prijs
+ * 
+ * bereken bestelling(aantalstuks + "stuk(s)" +artikels in hoofdletters)
+ * bereken totaalprijs  (eenheidsprijs x aantal stuks)
+ * bereken de prijs(kostprijs +totaalprijs als bedrag)
+ * 
+ * toon bestelling
+ * toon de prijs
+ */
+
+
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pcshop2._0inklas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
